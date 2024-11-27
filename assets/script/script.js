b2d0eab6d88ca971060dd72bf50c07016d749f5b
MARCELO RAMALHO DA SILVA GREGORIO <79231957+Marcelo-Ramalho@users.noreply.github.com>
2024-11-27 14:20:49 -0300

    script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -10,7 +10,7 @@ async function buscarStatus() {
     // Define a URL da API com base no ambiente
     const apiUrl = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
         ? 'http://192.168.0.169:5000' // Substitua pelo seu IP local
-        : 'https://db64-2804-14c-5bd8-40fc-cb0a-bce1-959f-e997.ngrok-free.app'; // URL pública fornecida
+        : 'https://7e71-2804-14c-5bd8-40fc-cb0a-bce1-959f-e997.ngrok-free.app'; // URL pública fornecida
 
     try {
         // Fazendo a requisição à API com autenticação básica
